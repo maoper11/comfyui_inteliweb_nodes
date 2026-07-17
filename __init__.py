ee22d3d68a10ee66c690e291d77c026bb7581a81
Mauricio Perdomo <32530656+maoper11@users.noreply.github.com>
2026-07-16 22:35:30 -0500

    Register Inteliweb Purge VRAM node

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
 from .system_check import InteliwebSystemCheck
+from .purge_vram import InteliwebPurgeVRAM
 
 NODE_CLASS_MAPPINGS = {
     "InteliwebSystemCheck": InteliwebSystemCheck,
+    "InteliwebPurgeVRAM": InteliwebPurgeVRAM,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "InteliwebSystemCheck": "System Check (Inteliweb)",
+    "InteliwebPurgeVRAM": "Purge VRAM (Inteliweb)",
 }
 
 import os as _os
