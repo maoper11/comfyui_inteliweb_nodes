7454739e12f67a82609353051153703b9d9541c1
Mauricio Perdomo <32530656+maoper11@users.noreply.github.com>
2026-07-22 00:55:40 -0500

    Register Replace Text Multi node

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from .system_check import InteliwebSystemCheck
 from .purge_vram import InteliwebPurgeVRAM
 from .input_switch import InteliwebInputSwitch
+from .replace_text_multi import InteliwebReplaceTextMulti
 
 # Registers the scanner-friendly /inteliweb/resource_monitor endpoint.
 from . import resource_monitor as _resource_monitor  # noqa: F401
@@ -9,12 +10,14 @@ NODE_CLASS_MAPPINGS = {
     "InteliwebSystemCheck": InteliwebSystemCheck,
     "InteliwebPurgeVRAM": InteliwebPurgeVRAM,
     "InteliwebInputSwitch": InteliwebInputSwitch,
+    "InteliwebReplaceTextMulti": InteliwebReplaceTextMulti,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "InteliwebSystemCheck": "System Check (Inteliweb)",
     "InteliwebPurgeVRAM": "Free Memory (Inteliweb)",
     "InteliwebInputSwitch": "Input Switch (Inteliweb)",
+    "InteliwebReplaceTextMulti": "Replace Text Multi - Inteliweb",
 }
 
 import os as _os
